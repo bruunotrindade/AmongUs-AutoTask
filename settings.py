fab0a2eeaa98789a322129a7a2ad7f73d227fbf3
Bruno Trindade <bruuno.trindade@gmail.com>
2020-11-03 22:11:07 -0500

    Configurações do reator

--- settings.py
+++ settings.py
@@ -1,11 +1,12 @@
 WIRE = 1
 CARD = 2
+REACTOR_NUMBERS = 3
 
 TASK_CHOICES = {
     WIRE: {
-        "LEFT_SIDE_COLOR": 542,
+        "LEFT_SIDE_COLOR": 562,
         "LEFT_SIDE_CLICK": 597,
-        "RIGHT_SIDE_COLOR": 1350,
+        "RIGHT_SIDE_COLOR": 1335,
         "RIGHT_SIDE_CLICK": 1276,
         "Y_POS": [271, 459, 646, 827],
         "KEY_PRESS": '1',
@@ -20,4 +21,17 @@ TASK_CHOICES = {
         "START_PIXEL_COLOR": (220, 220, 220),
         "START_PIXEL_POS": (824, 810)
     },
+    REACTOR_NUMBERS: {
+        "FRAME_MIN_X": 578,
+        "FRAME_MIN_Y": 388,
+        "FRAME_MAX_X": 1338,
+        "FRAME_MAX_Y": 688,
+        "SQUARE_SIZE": 135,
+        "SQUARES_MAX_POS": [
+            (585, 393), (738, 393), (892, 393),
+            (1045, 393), (1198, 393),   
+            (585, 550), (738, 550), (892, 550),
+            (1045, 550), (1198, 550)
+        ]
+    }
 }
