2791601a5ba756bf9db646b759389e2a4c010384
italo <italo.oliveira300@hotmail.com>
2020-11-04 15:36:18 -0300

    Add OCR in Reactor Task

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pyautogui
-from PIL import ImageGrab
+from PIL import ImageGrab, Image, ImageOps
 import time
 import keyboard
 from settings import *
+import tensorflow.keras
+import numpy as np
+import datetime
+
+def load_model_ocr():
+    print('Carregando o modelo do OCR...')
+    np.set_printoptions(suppress=True)
+    model = tensorflow.keras.models.load_model('./keras_model.h5')
+    print('Modelo carregado...')
+    return model
+
 
 def compare_pixel_color(image, pixels, colorMin, colorMax=None):
     R, G, B = image.getpixel(pixels)
@@ -13,6 +24,8 @@ def compare_pixel_color(image, pixels, colorMin, colorMax=None):
         result = result and (R <= colorMin[0] and G <= colorMin[1] and B <= colorMin[2])
 
     return result
+
+model_ocr = load_model_ocr()
 
 while True:
     if keyboard.is_pressed(TASK_CHOICES[WIRE]["KEY_PRESS"]):
@@ -52,7 +65,7 @@ while True:
             pyautogui.click(TASK_CHOICES[CARD]["HIDE_CARD_POS"])
             time.sleep(0.6)
             pyautogui.moveTo(TASK_CHOICES[CARD]["CARD_POS_START"])
-            pyautogui.dragTo(TASK_CHOICES[CARD]["CARD_POS_END"], duration=1.4, button="left", 
+            pyautogui.dragTo(TASK_CHOICES[CARD]["CARD_POS_END"], duration=1.0, button="left", 
                 tween=pyautogui.easeInOutQuad)
         else:
             print("CARTÃO NÃO ENCONTRADO")
@@ -60,10 +73,37 @@ while True:
 
     elif keyboard.is_pressed(TASK_CHOICES[REACTOR_NUMBERS]["KEY_PRESS"]):
         size = TASK_CHOICES[REACTOR_NUMBERS]["SQUARE_SIZE"]
-        ind = 0
+        ind = 1
+        positions = {}
+
         for square in TASK_CHOICES[REACTOR_NUMBERS]["SQUARES_MIN_POS"]:
-            #ImageGrab.grab(bbox=(square[0], square[1], square[0]+size, square[1]+size)).save("img"+str(ind)+".jpg", "JPEG")
+            #ImageGrab.grab(bbox=(square[0], square[1], square[0]+size, square[1]+size)).save("img"+str(ind)+"-"+str(datetime.datetime.now().timestamp())+".jpg", "JPEG")
+            image = ImageGrab.grab(bbox=(square[0], square[1], square[0]+size, square[1]+size))
             ind += 1
+
+            data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+            size_img = (224, 224)
+            image = ImageOps.fit(image, size_img, Image.ANTIALIAS)
+
+            image_array = np.asarray(image)
+            normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+            data[0] = normalized_image_array
+
+            prediction = model_ocr.predict(data)
+            number = np.argmax(prediction)
+            print(f'Predicted Number: {number + 1}')
+
+            middle_size = size / 2
+            pos_click = (square[0] + middle_size, square[1] + middle_size)
+
+            positions[number] = pos_click
+        
+        # Clicando na ordem certa
+        for i in range(10):
+            print(f'Clicando em: {i+1}')
+            pyautogui.click(positions[i])
+            time.sleep(0.1)
 
 
 
